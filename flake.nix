a64889b8a5ba24628d434e1d687a2700cc38fc45
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-11-10 16:36:09 +0100

    feat: crane flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      craneLib = crane.lib.${system};
+      pkgs = (import nixpkgs) {
+        inherit system;
+      };
+    in {
+      packages.default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource ./.;
+
+        # Add extra inputs here or any other derivation settings
+        # doCheck = true;
+        buildInputs = with pkgs; [freetype fontconfig wayland];
+        nativeBuildInputs = with pkgs; [cmake pkg-config];
+      };
+    });
+}
